3433361232e57c6ba6c6b9c5670f4c9a2d706b45
Wael <wael.hawa86@gmail.com>
2021-07-12 11:02:54 -0400

    First Commit

---
+++ MovieEFLab/MovieEFLab/Controllers/MovieLabController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieEFLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieEFLab.Controllers
+{
+    public class MovieLabController : Controller
+    {
+        private readonly MovieLabContext _context;
+        //using Day14._2_EFCore_DatabaseFirst.Models;
+
+        public MovieLabController(MovieLabContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,35 @@
+--CREATE DATABASE MovieLab;
+
+--USE MovieLab;
+
+--CREATE TABLE Movie
+--(
+--Id INT IDENTITY (1,1),
+--Title NVARCHAR(50),
+--Genre NVARCHAR (20),
+--Runtime FLOAT
+--);
+
+--ALTER TABLE Movie
+--ADD PRIMARY KEY (Id);
+
+--INSERT INTO Movie (Title,Genre)
+--VALUES
+--('Movie1', 'Genre1'),
+--('Movie2', 'Genre1'),
+--('Movie3', 'Genre1'),
+--('Movie4', 'Genre1'),
+--('Movie5', 'Genre1'),
+--('Movie6', 'Genre2'),
+--('Movie7', 'Genre2'),
+--('Movie8', 'Genre2'),
+--('Movie9', 'Genre2'),
+--('Movie10', 'Genre2'),
+--('Movie11', 'Genre3'),
+--('Movie12', 'Genre3'),
+--('Movie13', 'Genre3'),
+--('Movie14', 'Genre3'),
+--('Movie15', 'Genre3');
+
+--SELECT *
+--FROM Movie
